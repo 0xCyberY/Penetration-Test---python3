ef4c3ed806b080e07508125c25b8d1a8e0821189
ABDULLAH BAGHUTH <36686812+abdullah-baghuth@users.noreply.github.com>
2020-06-21 13:16:15 +0000

    source code

---
+++ nmapscanner.py
@@ -0,0 +1,18 @@
+import nmap
+
+scanner = nmap.PortScanner()
+target = input("Enter the target address: ")
+scanner.scan(target,"1-1024","-sV")
+
+print("The host name is " + scanner[target].hostname())
+print("The host status is " + scanner[target].state())
+
+keys = scanner[target]['tcp'].keys()
+for i in keys:
+    print('----------------')
+    print('the port'+str(i)+':')
+    res = scanner[target]['tcp'][i]
+    for n in res:
+        print(n + ':' + res[n])
+
+        
