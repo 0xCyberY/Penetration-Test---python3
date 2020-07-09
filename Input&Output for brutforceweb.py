0ae5c18391c7c44cd2b35fcfb236d83d710d07b2
ABDULLAH BAGHUTH <36686812+abdullah-baghuth@users.noreply.github.com>
2020-07-09 01:58:10 -0400

    Create Input&Output for brutforceweb.py

---
+++ Input&Output for brutforceweb.py
@@ -0,0 +1,13 @@
+please inter a URL: http://192.168.80.132 
+The page http://192.168.80.132/index
+is available
+The page http://192.168.80.132/test
+is available
+The page http://192.168.80.132/twiki
+is available
+The page http://192.168.80.132/tikiwiki
+is available
+The page http://192.168.80.132/
+is available
+The page http://192.168.80.132/phpinfo
+is available
